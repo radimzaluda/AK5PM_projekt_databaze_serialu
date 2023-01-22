ce40348bbf55c7563d138804e853dec98c2f39f7
Radim Zaluda <r_zaluda@utb.cz>
2023-01-22 09:30:26 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AK5PM_projekt_databaze_serialu',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
