481d7b9eac98f9a6626fd9279a14dc55ba7fdf81
Radim Zaluda <r_zaluda@utb.cz>
2023-01-23 20:37:46 +0100

    funkční výpis filmů+scrollování+připráva na 2. obrazovku (detailní info o daném filmu)

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,51 +1,47 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-
-
- //vygenerováno z wwww stránek http://json2ts.com/
-  export interface Result {
-      adult: boolean;
-      backdrop_path: string;
-      genre_ids: number[];
-      id: number;
-      original_language: string;
-      original_title: string;
-      overview: string;
-      popularity: number;
-      poster_path: string;
-      release_date: string;
-      title: string;
-      video: boolean;
-      vote_average: number;
-      vote_count: number;
-  }
-
-  export interface RootObject {
-      page: number;
-      results: Result[];
-      total_pages: number;
-      total_results: number;
-  }
-
-
+//vygenerováno z wwww stránek http://json2ts.com/
+export interface Result {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+export interface ApiResult {
+  page: number;
+  results: any[];
+  total_pages: number;
+  total_results: number;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getNejlepeHodnoceneFilmy(page = 1) : Observable<RootObject>{
-
-    return this.http.get<RootObject>
-    (`${environment.baseUrl}/movie/popular?api_key=${environment.apiKey}&page=${page}`);
+  getNejlepeHodnoceFilmy(page = 1): Observable<ApiResult> {
+    return this.http.get<ApiResult>(
+      `${environment.baseUrl}/movie/popular?page=${page}&api_key=${environment.apiKey}`
+    );
   }
 
-   getFilmInfo(id: string){
-  return this.http.get(`${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}`);
+  getFilmInfo(id: string): Observable<any> {
+    return this.http.get<ApiResult>(
+      `${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}`
+    );
   }
 }
